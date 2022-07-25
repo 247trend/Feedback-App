16cd90173897ab9c723bdf44ac29ef76f048ac2b
Jason Yuan <jasonyuan0130@gmail.com>
2022-07-26 03:48:22 +0930

    minor fix

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,7 +1,7 @@
 const FeedbackData = [
-  { id: 1, rating: 9, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at quam ipsum. Nullam cursus, turpis nec viverra malesuada, ligula dolor lacinia ligula, at convallis neque purus nec nibh. Vestibulum id ante quis tortor scelerisque pretium non id tellus. Phasellus fringilla convallis lorem quis egestas." },
-  { id: 2, rating: 10, text: "Sed ac molestie risus. Suspendisse sed lobortis ligula. Integer scelerisque volutpat ullamcorper. Vestibulum libero elit, dignissim eget dolor at, congue volutpat nisi." },
-  { id: 3, rating: 7, text: "Praesent viverra nisi sit amet mauris lacinia viverra. In vel neque ut leo vulputate hendrerit at ac lorem. Etiam laoreet arcu ac diam aliquam, vitae varius nisl aliquam." },
+  { id: 1, rating: 9, text: "This is a feedback example." },
+  { id: 2, rating: 10, text: "You can add new feedback." },
+  { id: 3, rating: 7, text: "You can also delete/edit existing feedback." },
 ]
 
 export default FeedbackData
