be74d07f196b322f935ad004dd47227c6bde1f06
Jason Yuan <jasonyuan0130@gmail.com>
2022-07-26 19:27:27 +0930

    delete/update feedback from backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -23,8 +23,11 @@ export const FeedbackProvider = ({ children }) => {
     edit: false,
   })
 
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`/feedback/${id}`, {
+      method: "DELETE",
+    })
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
@@ -50,8 +53,18 @@ export const FeedbackProvider = ({ children }) => {
     })
   }
 
-  const updateFeedback = (id, updItem) => {
-    setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item)))
+  const updateFeedback = async (id, updItem) => {
+    const res = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(updItem)
+    })
+
+    const data = await res.json()
+
+    setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...data } : item)))
   }
 
   return (
